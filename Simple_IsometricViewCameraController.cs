9f1d9047e713a48e55e25bb9e7dfeb2c0e06dda9
dfloaiza <dfloaiza@gmail.com>
2018-06-23 21:44:44 -0500

    Add files via upload

---
+++ Simple_IsometricViewCameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+  
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Camera.main.orthographicSize += 1; //Change values according to your requirements
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.orthographicSize -= 1;
+        }
+    }
+}
